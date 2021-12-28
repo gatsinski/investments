7169cc3b66ee970db90b37bf4883cdfc79cfc283
Hristo Gatsinski <gatsinski@gmail.com>
2021-12-28 15:02:14 +0200

    Add payment charts

--- investments/contrib/payments/admin.py
+++ investments/contrib/payments/admin.py
@@ -1,9 +1,25 @@
+import json
+
 from django.contrib import admin
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models import CharField, F, Sum, Value
+from django.db.models.functions import (
+    Concat,
+    ExtractDay,
+    ExtractMonth,
+    ExtractQuarter,
+    ExtractYear,
+    TruncDay,
+    TruncMonth,
+    TruncQuarter,
+)
+from django.shortcuts import render
 from django.urls import reverse
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 
 from investments.contrib.positions.models import Position
+from investments.contrib.securities.constants import SECTOR_CHOICES
 from investments.contrib.securities.models import Bond
 from investments.contrib.tags.models import Tag
 
@@ -53,6 +69,12 @@ class BasePaymentsAdmin(admin.ModelAdmin):
     )
     search_fields = ("position__security", "position__broker", "notes")
     autocomplete_fields = ("position",)
+    actions = [
+        "show_daily_payments",
+        "show_monthly_payments",
+        "show_quarterly_payments",
+        "show_securities_by_received_amount",
+    ]
 
     @admin.display(
         ordering="position",
@@ -134,6 +156,134 @@ class BasePaymentsAdmin(admin.ModelAdmin):
             )
         )
 
+    @admin.action(description=_("Show payments grouped by days"))
+    def show_daily_payments(self, request, queryset):
+        queryset = (
+            queryset.order_by()
+            .annotate(
+                day=ExtractDay("recorded_on"),
+                month=ExtractMonth("recorded_on"),
+                year=ExtractYear("recorded_on"),
+            )
+            .values("day", "month", "year")
+            .annotate(
+                value=Sum("amount"),
+                label=Concat(
+                    "day",
+                    Value("."),
+                    "month",
+                    Value("."),
+                    "year",
+                    output_field=CharField(),
+                ),
+            )
+            .order_by(TruncDay("recorded_on"))
+        )
+        return self.show_payments(
+            request,
+            queryset,
+            chart_name=_("Payments grouped by days"),
+            chart_label=_("Payments"),
+        )
+
+    @admin.action(description=_("Show payments grouped by months"))
+    def show_monthly_payments(self, request, queryset):
+        queryset = (
+            queryset.order_by()
+            .annotate(
+                month=ExtractMonth("recorded_on"), year=ExtractYear("recorded_on")
+            )
+            .values("month", "year")
+            .annotate(
+                value=Sum("amount"),
+                label=Concat("month", Value("."), "year", output_field=CharField()),
+            )
+            .order_by(TruncMonth("recorded_on"))
+        )
+        return self.show_payments(
+            request,
+            queryset,
+            chart_name=_("Payments grouped by months"),
+            chart_label=_("Payments"),
+        )
+
+    @admin.action(description=_("Show payments grouped by quarters"))
+    def show_quarterly_payments(self, request, queryset):
+        queryset = (
+            queryset.order_by()
+            .annotate(
+                quarter=ExtractQuarter("recorded_on"), year=ExtractYear("recorded_on")
+            )
+            .values("quarter", "year")
+            .annotate(
+                value=Sum("amount"),
+                label=Concat("quarter", Value("/"), "year", output_field=CharField()),
+            )
+            .order_by(TruncQuarter("recorded_on"))
+        )
+
+        return self.show_payments(
+            request,
+            queryset,
+            chart_name=_("Payments grouped by quarters"),
+            chart_label=_("Payments"),
+        )
+
+    @admin.action(description=_("Show payments grouped by years"))
+    def show_yearly_payments(self, request, queryset):
+        queryset = (
+            queryset.order_by()
+            .annotate(year=ExtractYear("recorded_on"))
+            .values("year")
+            .annotate(
+                value=Sum("amount"),
+                label=F("year"),
+            )
+            .order_by(TruncYear("recorded_on"))
+        )
+
+        return self.show_payments(
+            request,
+            queryset,
+            chart_name=_("Payments grouped by years"),
+            chart_label=_("Payments"),
+        )
+
+    def show_payments(self, request, queryset, chart_name, chart_label):
+        return render(
+            request,
+            "admin/chart.html",
+            context={
+                **self.admin_site.each_context(request),
+                "opts": self.model._meta,
+                "data": json.dumps(list(queryset), cls=DjangoJSONEncoder),
+                "chart_name": chart_name,
+                "chart_label": chart_label,
+                "chart_type": "bar",
+            },
+        )
+
+    @admin.action(description=_("Show securities grouped by received amount"))
+    def show_securities_by_received_amount(self, request, queryset):
+        queryset = (
+            queryset.order_by()
+            .values("position__security__name")
+            .annotate(value=Sum("amount"), label=F("position__security__name"))
+        )
+
+        return render(
+            request,
+            "admin/chart.html",
+            context={
+                **self.admin_site.each_context(request),
+                "opts": self.model._meta,
+                "data": json.dumps(list(queryset), cls=DjangoJSONEncoder),
+                "chart_name": _("Securities grouped by received amount"),
+                "chart_label": _("Received amount"),
+                "chart_type": "pie",
+            },
+        )
+
     def get_queryset(self, request):
         return (
             super()
@@ -164,6 +314,14 @@ class BasePaymentsAdmin(admin.ModelAdmin):
 
 @admin.register(DividendPayment)
 class DividendPaymentsAdmin(BasePaymentsAdmin):
+    actions = [
+        "show_daily_payments",
+        "show_monthly_payments",
+        "show_quarterly_payments",
+        "show_securities_by_received_amount",
+        "show_sectors_by_received_amount",
+    ]
+
     def get_list_display(self, request):
         list_display = list(self.list_display)
 
@@ -177,6 +335,29 @@ class DividendPaymentsAdmin(BasePaymentsAdmin):
         list_filter.insert(1, "type")
 
         return list_filter
+
+    @admin.action(description=_("Show sectors grouped by received amount"))
+    def show_sectors_by_received_amount(self, request, queryset):
+        queryset = (
+            queryset.order_by()
+            .values("position__security__stock__sector")
+            .annotate(value=Sum("amount"), label=F("position__security__stock__sector"))
+        )
+
+        label_map = {key: value for (key, value) in SECTOR_CHOICES}
+
+        return render(
+            request,
+            "admin/chart.html",
+            context={
+                **self.admin_site.each_context(request),
+                "opts": self.model._meta,
+                "data": json.dumps(list(queryset), cls=DjangoJSONEncoder),
+                "label_map": label_map,
+                "chart_name": _("Sectors grouped by received amount"),
+                "chart_type": "pie",
+            },
+        )
 
 
 @admin.register(InterestPayment)
