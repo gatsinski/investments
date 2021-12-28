ac9cf559c8adae3322782cc28144f2c248c7b506
Hristo Gatsinski <gatsinski@gmail.com>
2021-12-29 00:49:17 +0200

    Load the administration by default

--- investments/urls.py
+++ investments/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = i18n_patterns(
-    path("admin/", admin.site.urls),
+    path("", admin.site.urls),
 )
 
 if settings.DEBUG:
