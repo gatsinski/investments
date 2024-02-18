bffdd16a00070175242e8986d08549ea5a7dedcb
Hristo Gatsinski <gatsinski@gmail.com>
2024-02-18 19:57:59 +0200

    Convert amounts in position report using exchange reate on correct date

--- investments/templatetags/rates.py
+++ investments/templatetags/rates.py
@@ -6,12 +6,19 @@ register = template.Library()
 
 @register.simple_tag
 def get_local_currency_rate(date, rates):
+    if not date:
+        return ""
+
     rate_instance = rates.get(date.isoformat())
 
     if not rate_instance:
         return _("No exchange rate found")
 
     return rate_instance.rate
+
+@register.simple_tag
+def calcualte_profit_or_loss_in_local_currency(model, open_rate, close_rate):
+    return round(model["close_amount"] * close_rate, 2) - round(model["open_amount"] * open_rate, 2) if model["close_amount"] else None
 
 
 @register.filter
